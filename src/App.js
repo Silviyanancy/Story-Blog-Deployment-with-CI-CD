80a0cca801c6881683cdf2d864924baf587b896f
Silviya Nancy J <jsilviyanancy@gmail.com>
2024-03-08 10:28:54 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import React from 'react';
 import './App.css';
-
+ 
 function App() {
-  return (
-    <div className="App">
-      Hello, First Milestone
-    </div>
-  );
+    return (
+        <h1> Hello World! </h1>
+    );
 }
-
+ 
 export default App;
