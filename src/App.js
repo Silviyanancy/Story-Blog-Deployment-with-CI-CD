a58cb829692c7a648b06a3b735aa3f2a9339fae2
Silviyanancy <jsilviyanancy@gmail.com>
2024-03-20 18:45:37 -0600

    Addition to App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       Hello, First Milestone for the devops project
       <h2>Hello class Good evening</h2>
+      <h3>I am starting a story app today with firebase</h3>
     </div>
   );
 }
